e8cec82322a312fc26997badbb300983213d9d72
lgernand <landongernand@outlook.com>
2025-12-04 00:46:13 -0600

    added reactive form to accept new pets

--- src/app/components/user-profile/pet/pet.component.ts
+++ src/app/components/user-profile/pet/pet.component.ts
@@ -1,25 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Pet } from '../../../models/pet';
 import { PetService } from '../../../services/pet.service';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-pet',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './pet.component.html',
   styleUrl: './pet.component.css'
 })
 export class PetComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  
   constructor(authService: AuthService, private petService: PetService) {
 
   }
 
-  pets: Pet[] = [];
+  addPetForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    dateOfBirth: [new Date(Date.now.toString()), Validators.required],
+    gender: ['', Validators.required],
+    species: ['', Validators.required],
+    breed: ['', Validators.required]
+  });
+
+  pets: Pet[] = [];  
 
   ngOnInit(): void {
     this.petService.getPets().subscribe(response => {
       this.pets = response;
     });    
+  }
+
+  handleSubmit(): void {
+    console.warn('submitting ' + this.addPetForm.value.name)
+    let newPet: Pet = this.addPetForm.value as Pet;
+
+    this.petService.createProfile(newPet).subscribe(() => {
+      this.petService.getPets().subscribe(response => {
+        this.pets = response;
+        this.addPetForm.reset()
+      })
+    })
   }
 }
--- src/app/models/pet.ts
+++ src/app/models/pet.ts
@@ -1,7 +1,8 @@
 export interface Pet {
-    id: string;
-    owner: string;
+    id?: string;
+    owner?: string;
     name: string;
+    gender: string;
     species: string;
     breed: string;
     dateOfBirth: Date
